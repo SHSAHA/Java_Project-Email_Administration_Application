db131253c1ea032373ee34df6209911fe4bfd232
Shaswata Saha <shsaha@ttu.ee>
2019-05-18 13:05:52 +0300

    Files

---
+++ Email/src/emailapp/EmailApp.java
@@ -0,0 +1,15 @@
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		Email emp1 = new Email("Shaswata","Saha");
+		
+		//emp1.setAlternateEmail("shaswatasaha24@gmail.com");
+		//System.out.println(emp1.getAlternateEmail());
+		System.out.println(emp1.showInfo());
+		
+
+	}
+
+}
